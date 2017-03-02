0a3cb05a6d4f13b76ad986fc676fb79adf981dc3
superuser mintkde <mintkde@reyesaguayo.com>
2017-03-02 15:43:55 -0600

    add Matlab function to get .mat file info.
    add R function to get data frame of .mat file objects

--- R/functions.R
+++ R/functions.R
@@ -1,4 +1,5 @@
 library(R.matlab)
+library(RcppOctave)
 
 readMatfile <- function(matfile, folder = project.extdata, verbose = FALSE) {
   matfileLong <- paste(folder, matfile, sep = "/")
@@ -10,4 +11,19 @@ readMatfile <- function(matfile, folder = project.extdata, verbose = FALSE) {
 savetoRda <- function(..., file, folder = project.data) {
   rdafileLong <- paste(folder, file, sep = "/")
   save(..., file = rdafileLong)
+}
+
+
+o.loader <- OctaveFunction("
+function [struct] = readMatfile(mfile)
+  load(mfile)
+  struct = whos();
+end
+")
+
+matInfo <- function(matFile) {
+  ld <- o.loader(matFile)
+  sz <- paste(sapply(ld$size, `[[`, 1), sapply(ld$size, `[[`, 2), sep = "x")
+  df <- data.frame(name = ld$name, size = sz, bytes = ld$bytes, class = ld$class)
+  return(df)
 }

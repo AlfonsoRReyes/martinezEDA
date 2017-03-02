575414c0771b677a518e599a1a84ec19469a81c0
superuser mintkde <mintkde@reyesaguayo.com>
2017-03-02 07:56:02 -0600

    do not print content of list mat file

--- R/functions.R
+++ R/functions.R
@@ -3,7 +3,7 @@ library(R.matlab)
 readMatfile <- function(matfile, folder = project.extdata) {
   matfileLong <- paste(folder, matfile, sep = "/")
   ret <- readMat(matfileLong, fixNames = TRUE)
-  print(str(ret))
+  # print(str(ret))
   return(ret)
 }
 

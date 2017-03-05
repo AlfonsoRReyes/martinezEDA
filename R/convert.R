0b52d0fc5b7653a356e31e3cb82e0347bf3b688f
superuser mintkde <mintkde@reyesaguayo.com>
2017-03-05 01:52:15 -0600

    integrated functions working but with two global variables: mf and matList

--- R/convert.R
+++ R/convert.R
@@ -1,3 +1,60 @@
+library(R.matlab)
+library(RcppOctave)
+
+
+readMatfile <- function(matfile, folder = project.extdata, verbose = FALSE) {
+  matfileLong <- paste(folder, matfile, sep = "/")
+  # print(matfileLong)
+  ret <- readMat(matfileLong, fixNames = FALSE)
+  if (verbose) print(str(ret))
+  return(ret)
+}
+
+
+
+
+#' get information of .mat file objects
+#' as a data frame
+matInfo <- function(matfile) {
+  matfilepath <- paste(project.extdata, matfile, sep = "/")
+  ld <- o.loader(matfilepath)
+  if (typeof(ld$size) == 'list') {
+    sz <- paste(sapply(ld$size, `[[`, 1), sapply(ld$size, `[[`, 2), sep = "x")
+  } else {
+    sz <- paste(sapply(ld$size, `[[`, 1), collapse = "x")
+  }
+  # sz <- paste(sapply(ld$size, `[[`, 1), sapply(ld$size, `[[`, 2), sep = "x")
+  df <- data.frame(name = ld$name,
+                   size = sz,
+                   bytes = ld$bytes,
+                   class = ld$class,
+                   stringsAsFactors = FALSE)
+  df <- df[order(df[, 1]), ]
+  rownames(df) <- NULL
+  return(df)
+}
+
+
+
+
+savetoRda <- function(..., file, folder = project.data) {
+  rdafileLong <- paste(folder, file, sep = "/")
+  save(..., file = rdafileLong)
+}
+
+
+#' load .mat file in Octave
+#' and get basic info
+o.loader <- OctaveFunction("
+function [struct] = readMatfile(mfile)
+  load(mfile)
+  clear mfile;
+  struct = whos();
+end
+")
+
+
+
 #' split a 1-D matrix in a 2-D matrix by means of the # columns
 split.matrix <- function(mat, cols) {
   rows <- length(mat) / cols
@@ -13,6 +70,8 @@ split.matrix <- function(mat, cols) {
 
 
 process.matlab.object <- function(item) {
+  # matList <- get('matList', envir = parent.env(e))
+
   kls <- as.character(mf[mf$name == item, "class"]) # get object class
   size <- as.character(mf[mf$name == item,][1, 'size'])
   dim <- as.integer(unlist(strsplit(size, "x")))
@@ -57,26 +116,87 @@ process.matlab.object <- function(item) {
 
 
 # main
-rdaFile <- paste(unlist(strsplit(matfile, "\\."))[1], "rda",sep = ".")
-toSave <- NULL
-rdf <- data.frame()
+# rdaFile <- paste(unlist(strsplit(matfile, "\\."))[1], "rda",sep = ".")
+# toSave <- NULL
+# rdf <- data.frame()
+#
+# for (item in mf$name) {
+#   # cat(item, class(item), "\n")
+#   cat(item, "\t")
+#   #print(process.matlab.object(item)$Rname)
+#
+#   # row <- process.matlab.object(item)
+#   whole <- process.matlab.object(item)
+#   assign(whole$name, whole$values)
+#   row <- whole$properties
+#   cat(row$Rclass, row$typeof)
+#   row.df <- data.frame(row, stringsAsFactors = FALSE)
+#   rdf <- rbind(row.df, rdf)
+#   cat("\n")
+#   toSave <- c(toSave, whole$name)
+# }
+# rdf <- rdf[order(rdf[, 1]), ]
+# rownames(rdf) <- 1:nrow(rdf)
+# print(rdf)
+# savetoRda(list = toSave, file = rdaFile)
 
-for (item in mf$name) {
-  # cat(item, class(item), "\n")
-  cat(item, "\t")
-  #print(process.matlab.object(item)$Rname)
 
-  # row <- process.matlab.object(item)
-  whole <- process.matlab.object(item)
-  assign(whole$name, whole$values)
-  row <- whole$properties
-  cat(row$Rclass, row$typeof)
-  row.df <- data.frame(row, stringsAsFactors = FALSE)
-  rdf <- rbind(row.df, rdf)
-  cat("\n")
-  toSave <- c(toSave, whole$name)
+mat2rda <- function(matfile) {
+  # create new environment
+  e <- new.env()
+
+  # provide the Matlab file name
+  matfilepath <- paste(project.extdata, matfile, sep = "/")
+
+  # stop if Matlab file does not exist
+  if (!file.exists(matfilepath)) stop("file does not exist ...")
+
+  # get info from the Matlab file using RcppOctave
+  mf <- matInfo(matfile)
+  assign('mf', mf, envir = .GlobalEnv)
+
+  # read the Matlab file from R
+  matList <- readMatfile(matfile)   # load the Matlab file results to a list
+  print(names(matList))
+  assign('matList', matList, envir = .GlobalEnv)
+
+  # iterate through objects in data frame returned from RcppOctave
+  rdaFile <- paste(unlist(strsplit(matfile, "\\."))[1], "rda",sep = ".")
+  toSave <- NULL
+  rdf <- data.frame()
+
+  for (item in mf$name) {
+    # cat(item, class(item), "\n")
+    cat(item, "\t")
+    #print(process.matlab.object(item)$Rname)
+
+    # row <- process.matlab.object(item)
+    whole <- process.matlab.object(item)
+    assign(whole$name, whole$values)
+    row <- whole$properties
+    cat(row$Rclass, row$typeof)
+    row.df <- data.frame(row, stringsAsFactors = FALSE)
+    rdf <- rbind(row.df, rdf)
+    cat("\n")
+    toSave <- c(toSave, whole$name)
+  }
+  rdf <- rdf[order(rdf[, 1]), ]
+  rownames(rdf) <- 1:nrow(rdf)
+  print(rdf)
+  savetoRda(list = toSave, file = rdaFile, envir = parent.env(e))
+
+
+
+  # convert object name item to character
+  # get the class and dimension for current object
+  # if class is `cell`
+  #   stop if any of the dimension of cell is greater than 1
+  #   unlist the object before assigning to a var
+  #   otherwise assign the object to a var with `assign`
+  # collect the variable names to save later
+
+  # save the objects as .rda file
+
+  # create data frame of new objects in R
+  # show data frame of objects in Matlab and R
 }
-rdf <- rdf[order(rdf[, 1]), ]
-rownames(rdf) <- 1:nrow(rdf)
-print(rdf)
-savetoRda(list = toSave, file = rdaFile)

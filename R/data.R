c92a5f39dab5887b00df3c06eca93cb89476be9c
superuser mintkde <mintkde@reyesaguayo.com>
2017-03-05 22:54:47 -0600

    add documentation for data files

---
+++ R/data.R
@@ -0,0 +1,302 @@
+#' singer.rda
+#' @docType data
+#'
+"Alto_1"
+#' Alto_2
+"Alto_2"
+#' Bass_1
+"Bass_1"
+#' Bass_2
+"Bass_2"
+#' Soprano_1
+"Soprano_1"
+#' Soprano_2
+"Soprano_2"
+#' numeric, 1 column
+"Tenor_1"
+#' numeric, 1 column
+"Tenor_2"
+
+
+#' ethanol.rda
+#'
+#' num, 1x88
+"Compression"
+#' num 1x88
+"Equivalence"
+#' num, 1x88
+"NOx"
+#' char
+"comment"
+
+#' abrasion.rda
+#'
+#' num, 1x30
+"abrasion"
+#' num, 1x30
+"hardness"
+#' num, 1x30
+"tensile"
+
+#' bank.rda
+#'
+#' num, 100x6
+"forge"
+#' num, 100x6
+"genuine"
+
+#' calibrat.rda
+#'
+#' num, 14x1
+"counts"
+#' num, 14x1
+"tsh"
+
+#' ceral.rda
+"cereal"
+"labs"
+
+
+#' data.rda
+"X"
+
+#' environmental.rda
+#'
+#' num, 1x111
+"Ozone"
+#' num, 1x111
+"SolarRadiation"
+#' num, 1x111
+"Temperature"
+#' num, 1x111
+"WindSpeed"
+
+#' forearm.rda
+"forearm"
+
+#' galaxy.rda
+#'
+#' num, 323x1
+"EastWest"
+#' num, 323x1
+"NorthSouth"
+#' num, 323x1
+"Velocity"
+
+#' geyser.rda
+"geyser"
+
+#' hamster.rda
+"labs"
+"X"
+
+#' iradbpm.rda
+#'
+#' num 503x1
+"classlab"
+#' matrix 503x503
+"iradbpm"
+
+#' iris.rda
+#'
+#' num 50x4
+"setosa"
+#' num 50x4
+"versicolor"
+#' num 50x4
+"virginica"
+
+#' L1bpm.rda
+#'
+#' num 503x1
+"classlab"
+#' matrix 503x503
+"L1bpm"
+
+#' leukemia.rda
+#'
+#' char, 72x1
+"btcell"
+#' char, 72x1
+"cancertype"
+#' char, 50x1
+"geneinfo"
+#' char, 50x72
+"leukemia"
+
+
+#' livestock.rda
+#' num 1 x 130
+"Count"
+#' num 1 x 130
+"Country"
+#' char 1 x 26
+"CountryName"
+#' char 1 x 5
+"LivestockName"
+#' num 1 x 130
+"LivestockType"
+#' char
+"comment"
+
+#' lsiex.rda
+#'
+#' char 5x1
+"docs"
+#' num 6x5
+"termdoc"
+#' char 6x1
+"words"
+#' num 6x5
+"X"
+
+#' lungA.rda
+#'
+#' char 203x1
+"labA"
+#' matrix 3312x203
+"lungA"
+
+#' lungB.rda
+#'
+#' char 156x1
+"labB"
+#' matrix 675x156
+"lungB"
+
+#' matchbpm.rda
+#'
+#' num 503x1
+"classlab"
+#' matrix 503x503
+"matchbpm"
+
+#' ochiaibpm.rda
+#'
+#' num 503x1
+"classlab"
+#' matrix 503x503
+"ochiaibpm"
+
+#' oronsay.rda
+#'
+#' num 226x1
+"beachdune"
+#' char 12x1
+"labcol"
+#' num 226x1
+"midden"
+#' num 226x12
+"oronsay"
+
+#' ozone.rda
+#'
+#' num 132x1
+"Stamford"
+#' num 132x1
+"Yonkers"
+#' char
+"comment"
+
+#' playfair.rda
+#' num 22x1
+"Diameter"
+#' char 22x1
+"labs"
+#' num 22x1
+"Population"
+#' char
+"comment"
+
+#' pollen.rda
+"data"
+
+#' posee.rda
+#'
+#' num 400x8
+"boite"
+#' num 400x8
+"croix"
+#' num 400x8
+"curve"
+#' num 400x8
+"groupe"
+#' num 400x8
+"spiral"
+#' num 400x8
+"struct2"
+
+#' salmon.rda
+#' @docType data
+"salmon"
+
+#' scurve.rda
+#'
+#' num 1x1000
+"angle"
+#' num 32x11
+"cc"
+#' num 1x1
+"d"
+#' num 1x2000
+"height"
+#' num 1x1
+"hh"
+#' 1x1
+"K"
+#' 1x1
+"N"
+#' 1x16
+"tt"
+#' 1x16
+"uu"
+#' 3x2000
+"X"
+#' num 32x11
+"xx"
+#' num 2x2000
+"Y"
+#' num 32x11
+"yy"
+#' num 32x11
+"zz"
+
+#' skulls.rda
+#'
+#' num 40x12
+"skullsdata"
+
+#' software.rda
+#'
+#' num 491x1
+"defpage"
+#' num 426x1
+"defsloc"
+#' num 426x1
+"mtgsloc"
+#' num 491x1
+"prepage"
+#' num 426x1
+"prepsloc"
+
+#' spam.rda
+#'
+#' matrix 4601x58
+"spam"
+#' num 4601x1
+"spamind"
+
+#' sparrow.rda
+"sparrow"
+
+#' stockreturns.rda
+"stocks"
+
+
+
+
+
+
+
+
+
